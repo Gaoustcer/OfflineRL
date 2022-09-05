24d2b7f04b12ca2c3c71388ab40dcd66ef8bf468
Gaoustcer <2337284889@qq.com>
2022-09-05 10:22:00 +0800

    generatedata

---
+++ dataconnect.py
@@ -0,0 +1,29 @@
+import gym
+
+import numpy as np
+
+def _connect():
+    env = gym.make("CartPole-v0")
+    currentstate = []
+    action = []
+    reward = []
+    nextstate = []
+    done = []
+    EPOSIDETIME = 1024
+    for epoch in range(EPOSIDETIME):
+        state = env.reset()
+        d = False
+        while d == False:
+            a = env.action_space.sample()
+            ns,r,d,_ = env.step(a)
+            currentstate.append(state)
+            action.append(a)
+            reward.append(r)
+            nextstate.append(ns)
+            done.append(d)
+    np.save("currentstate.npy",np.array(currentstate))
+    np.save('action.npy',np.array(action))
+    np.save('nextstate.npy',np.array(nextstate))
+    np.save('done.npy',np.array(done))
+if __name__ == "__main__":
+    _connect()
