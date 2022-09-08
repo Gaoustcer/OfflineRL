6a9dca315d2d922203775f27b81d07c32706322f
Gaoustcer <2337284889@qq.com>
2022-09-08 23:36:47 +0800

    commit loss log,whether loss without given weight reduce in given dataset

--- Q_learning_offline/train_offline_prob.py
+++ Q_learning_offline/train_offline_prob.py
@@ -16,10 +16,11 @@ class TrainingAgent(object):
         self.device = torch.device('cuda:0')
         self.Qnet = Net().to(self.device)
         self.TargetQnet = Net().to(self.device)
-        self.optimizer = torch.optim.Adam(self.Qnet.parameters(), lr = 5e-5)
+        self.optimizer = torch.optim.Adam(self.Qnet.parameters(), lr = 4e-4)
         self.epoch = 256
         self.validenv = gym.make(ENV)
         self.vaildationtime = 16
+        self.lossindex = 1
         self.mydataset = RLdataset(self.validenv.x_threshold,self.validenv.theta_threshold_radians)
         self.mydataloader = DataLoader(self.mydataset,batch_size=32)
 
@@ -39,6 +40,25 @@ class TrainingAgent(object):
             TDloss.backward()
             self.optimizer.step()
 
+    def _trainanepochprob(self):
+        from tqdm import tqdm
+        for currentstate,action,reward,nextstate in (self.mydataloader):
+            values = self.Qnet(currentstate)
+            # (s,r,a,s')
+            # Q(s,a) - \max_{a'} Q(s',a')+r
+            self.optimizer.zero_grad()
+            action = action.cuda()
+            currentQvalues = torch.gather(values,-1,action.unsqueeze(-1)).squeeze()
+            nextQvalues = torch.max(self.TargetQnet(nextstate),-1)[0].detach()
+            probnextQ = softmax(nextQvalues)
+            nextQvalues = torch.einsum('ij,ij->i',nextQvalues,probnextQ)
+            TDlossfunction = torch.nn.MSELoss()
+            TDloss = TDlossfunction(currentQvalues,nextQvalues)
+            # self.optimizer.zero_grad()
+            self.writer.add_scalar('TDloss',TDloss,self.lossindex)
+            self.lossindex += 1
+            TDloss.backward()
+            self.optimizer.step()
 
 
 
@@ -48,6 +68,7 @@ class TrainingAgent(object):
             done = False
             state = self.validenv.reset()
             while done == False:
+                # action = torch.argmax(self.Qnet(state)).item()
                 prob = softmax(self.Qnet(state))
                 action = Categorical(prob).sample().cpu().item()
                 ns,r,done,_ = self.validenv.step(action)
@@ -65,14 +86,14 @@ class TrainingAgent(object):
 
     def _train(self):
         from torch.utils.tensorboard import SummaryWriter
-        writer = SummaryWriter('../log/offlineQlearning1024')
+        self.writer = SummaryWriter('../log/offlineQlearning1024')
         from tqdm import tqdm
         for epoch in tqdm(range(self.epoch)):
             self._trainanepoch()
             if epoch % 8 == 0:
                 self.TargetQnet.load_state_dict(self.Qnet.state_dict())
             reward = self.vaildate()
-            writer.add_scalar('reward',reward,epoch)
+            self.writer.add_scalar('reward',reward,epoch)
 
 
 if __name__ == "__main__":
